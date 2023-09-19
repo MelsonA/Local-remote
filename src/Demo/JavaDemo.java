3b65273200f9d1dd010af3bd5330729022d31a48
melson.arockiya <melson.arockiya@jocata.com>
2023-09-19 13:32:55 +0530

    first commit

---
+++ src/Demo/JavaDemo.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class JavaDemo {
+	public void add(int a,int b) {
+		int result=a+b;
+		System.out.println(result);
+	}
+	
+	public void add(int a) {
+		System.out.println(a);
+	}
+
+}
